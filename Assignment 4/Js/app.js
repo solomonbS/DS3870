3939fa7af7ad40f6de5fbb1a889db4e6a762c33a
solomonbS <89319534+solomonbS@users.noreply.github.com>
2021-09-15 14:09:46 -0500

    assignment

--- Assignment 4/Js/app.js
+++ Assignment 4/Js/app.js
@@ -8,9 +8,9 @@ $.getJSON("https://www.swollenhippo.com/getEmployeesByAPIKey.php?APIKey=Mickey20
 
 function buildEmployeeCard(){
         $. each(arrEmployee,function(i,person){
-                let strHTML='<div class="card mt-5 col-6 offset-3">';
+                let strHTML='<div class="card mt-5 col-3 ">';
                 strHTML += '<h3 class="text-center mt-3"><a href="mailto: '+ person.Email + '"> ' + person.FirstName + ' ' + person.LastName +'</a></h3>';
-                strHTML += '<h4 class="text-center">' + person.person +' </h4>';
+                strHTML += '<h4 class="text-center">' + person.Postion  +' </h4>';
                 strHTML += '<h4 class="mt-3"> Profile Details </h4>';
                 strHTML += '<p> Hire Date: '+ person.HireDate + '</p>';
                 strHTML += '<p  class = "txtHourlyRate" data-rate= " '+ person.HourlyRate +'">Hourly Rate: ' + person.HourlyRate + '</p>';
@@ -34,7 +34,7 @@ function buildEmployeeCard(){
 $(document).on('click','.btnCalculatePay', function(){
 let decHours = $(this).closest('.card').find('.txtHours').val();
 console.log(decHours);
-let decRate = $(this).closest('.card').find('.txtHourlyRate').val().split (': ')[1];
+let decRate = $(this).closest('.card').find('.txtHourlyRate').text().split (': ')[1];
 console.log(decRate);
 $(this).closest('.card').find('.txtTotalPay').val(decHours * decRate);
 
